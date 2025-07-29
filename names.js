75feee68aa4b6aa88ba13f6e074177c252c39ee4
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-29 16:36:53 +0200

    feat: create names

--- names.js
+++ names.js
@@ -1,1 +1,8 @@
-console.log('ciao')
+//Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+// Esporta la funzione dal file.
+
+const names = (firstName , lastName) => ({firstName , lastName})
+
+console.log(names('carlo','martino'))
+
+module.exports = names
